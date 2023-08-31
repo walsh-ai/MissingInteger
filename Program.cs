6e5af617c1bed6ecd301af7d2128787ffb4c38a9
csnirs <walsh.jthomas@outlook.com>
2023-08-31 10:08:53 +0100

    First commit, solution written

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Top level statements
+// Test solution 
+int[] arr = {1, 3, 6, 4, 1, 2};
+Console.WriteLine($"Result of A = [1, 3, 6, 4, 1, 2]: {MinimumMissingInt(arr).ToString()}"); 
+
+public static partial class Program {
+    /// <summary>
+    /// Find the smallest positive integer (not 0) that is not
+    /// present in the given array.
+    /// This solution simply runs a quick sort on the array, and 
+    /// increments the smallest non-found integer when it is encountered. 
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <returns></returns>
+    public static int MinimumMissingInt(int[] arr) {
+        // Sort the input array (QuickSort)
+        Array.Sort(arr); 
+
+        // Find the minimum integer 
+        // Maintain the smallest positive integer not found
+        // Increment the smallest value when it is encountered 
+        int smallest = 1; 
+        foreach (int i in arr) {
+            if (i == smallest) {
+                smallest++; 
+            }
+        }
+
+        return smallest; 
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A. 
+
+E.g. given input A = {1, 3, 6, 4, 1, 2}, the solution should return 5. 
+
+A = [1, 2, 3], return 4 
+A = [-1, -3], return 1
+
+For this, an efficient algorithm is required under the following assumptions:(1) N is an integer in range [1, 100000]; (2) each element of A is an integer in the ragne [-1000000, 1000000]
